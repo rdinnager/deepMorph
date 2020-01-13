e1091d3a105b3aeb2a4a45eeaebe71c227e530dc
rdinnager <r.dinnage@gmail.com>
2020-01-13 17:22:07 +1100

    finished reorienting and cutting meshes using new method and calculating the sphere that containes all meshes where each mesh has a constant coordinate.

--- R/mesh_to_sdf.R
+++ R/mesh_to_sdf.R
@@ -2,108 +2,170 @@ library(Morpho)
 library(Rvcg)
 library(readobj)
 library(rgl)
+library(geometry)
+library(proxy)
 
-beak_files <- list.files("data/oriented_trimmed_meshes_obj_circle/", full.names = TRUE) 
+## Step one is to iterate over each mesh, reorient the beak by the landmarks, and then
+## rescale each beak to constant beak length. I will also save the convex hull of 
+## each resulting mesh to use in the next step.
 
-beak_file <- beak_files[16]
+beak_files <- list.files("data/objs/", full.names = TRUE) 
 
-beak_mesh <- try(readobj::read.obj(beak_file, convert.rgl = TRUE)[[1]])
-rgl::shade3d(beak_mesh)
+beak_file <- beak_files[1]
 
+clip_mesh_by_mesh <- function(mesh_to_clip, mesh_to_clip_with) {
+  #rgl::shade3d(mesh_to_clip, alpha = 0.5, col = "red")
+  #rgl::shade3d(mesh_to_clip_with, alpha = 0.5, col = "green")
+  dists <- Rvcg::vcgClostKD(t(mesh_to_clip$vb), mesh_to_clip_with)
+  clipped <- rgl::clipMesh3d(mesh_to_clip, dists$quality)
+  clipped
+  #rgl::shade3d(clipped)
+}
 
-reorient_beak <- function(beak_file, sdf_folder = "data/sdfs", plot_folder = NULL, save_torch = TRUE) {
+#beak_file <- beak_files[168]
+#plot_folder <- "beak_plots_trimmed"
+reorient_beak <- function(beak_file, mesh_folder = "data/oriented_scaled_meshes_obj",
+                          landmark_folder = "data/landmarks_oriented", hull_folder = "data/conv_hulls") {
   
-  if(!dir.exists(sdf_folder)) {
-    dir.create(sdf_folder)
+  if(!dir.exists(mesh_folder)) {
+    dir.create(mesh_folder)
+  }
+  if(!dir.exists(landmark_folder)) {
+    dir.create(landmark_folder)
   }
   
   bird_file <- basename(beak_file)
+
+  new_beak_file <- file.path(mesh_folder, bird_file)
+  hull_file <- file.path(hull_folder, gsub(".obj", ".csv", bird_file))
   
-  sdf_file <- file.path(sdf_folder, bird_file)
-  
-  if(!is.null(plot_folder)) {
-    if(!dir.exists(plot_folder)) {
-      dir.create(plot_folder)
-    }
+  if(!dir.exists(hull_folder)) {
+    dir.create(hull_folder)
   }
   
   
-  if(!file.exists(sdf_file)) {
+  
+  if(!file.exists(new_beak_file)) {
     
     beak_mesh <- try(readobj::read.obj(beak_file, convert.rgl = TRUE)[[1]])
+    suppressMessages(beak_landmarks <- try(readr::read_delim(file.path("data/landmarks/Markup_1", gsub(".obj", ".txt", bird_file, fixed = TRUE)), 
+                                                             " ",
+                                                             col_names = c("X", "Y", "Z"))))
     
     if(inherits(beak_mesh, 'try-error') | inherits(beak_landmarks, 'try-error')) {
-      bad_meshes <- unique(c(readr::read_lines("data/bad_mesh.txt"), bird_file))
-      readr::write_lines(bad_meshes, "data/bad_mesh.txt")
+      readr::write_lines(bird_file, "data/bad_models.txt", append = TRUE)
     } else {
       
-        mesh_pnts <- Morpho::vert2points(beak_mesh)
-        
-        mesh_cent <- apply(mesh_pnts, 2, mean, na.rm = TRUE)
-        furthest <- pdist::pdist(mesh_cent, 
-                                 mesh_pnts) %>%
-          as.matrix() %>%
-          max(na.rm = TRUE)
-        
-        scaled_mesh <- beak_mesh %>%
-          rgl::translate3d(-mesh_cent[1], -mesh_cent[2], -mesh_cent[3]) %>%
-          Morpho::scalemesh(1/furthest, center = "none")
-        
-        mesh_pnts <- Morpho::vert2points(scaled_mesh)
-        
-        max_x_pnt <- mesh_pnts[which.max(mesh_pnts[ , 1]), ]
-        furthest <- pdist::pdist(mesh_pnts[which.max(mesh_pnts[ , 1]), , drop = FALSE], 
-                                 mesh_pnts[-which.max(mesh_pnts[, 1]), ]) %>%
-          as.matrix() %>%
-          max(na.rm = TRUE)
-
-        
-        sphere <- Rvcg::vcgSphere(subdivision = 5) %>%
-          Morpho::scalemesh(furthest) %>%
-          rgl::translate3d(max_x_pnt[1], max_x_pnt[2], max_x_pnt[3])
-        
-        sdf <- Rvcg::vcgClostKD()
-        
-        rgl::shade3d(scaled_mesh, alpha = 0.5)
-        rgl::shade3d(Rvcg::vcgSphere(5), alpha = 0.6, col = "red")
-        rgl::shade3d(sphere, alpha = 0.6, col = "red")
-          
-        
-        if(!is.null(plot_folder)) {
-          
-          rgl::clear3d()
-          rgl::shade3d(cut_1, col = "red")
-          rgl::rgl.bringtotop()
-          rgl::rgl.viewpoint(userMatrix = matrix(c(0.64, 0.71, -0.27, 0,
-                                                   -0.32, 0.58, 0.74, 0,
-                                                   0.69, -0.38, 0.61, 0,
-                                                   0, 0, 0, 1), nrow = 4, byrow = TRUE),
-                             zoom = 0.8)
-          rgl::par3d(windowRect = c(20, 30, 800, 800))
-          
-          rgl::rgl.snapshot(file.path(plot_folder, gsub(".obj", ".png", bird_file, fixed = TRUE)))
-          
-        }
-        
-        
-      } else {
-        Rvcg::vcgObjWrite(rot$mesh, new_beak_file)
-        
-        if(!is.null(plot_folder)) {
-          rgl::clear3d()
-          rgl::shade3d(rot$mesh, col = "red")
-          rgl::rgl.bringtotop()
-          rgl::rgl.viewpoint(userMatrix = matrix(c(0.64, 0.71, -0.27, 0,
-                                                   -0.32, 0.58, 0.74, 0,
-                                                   0.69, -0.38, 0.61, 0,
-                                                   0, 0, 0, 1), nrow = 4, byrow = TRUE),
-                             zoom = 0.8)
-          rgl::par3d(windowRect = c(20, 30, 800, 800))
-          
-          rgl::rgl.snapshot(file.path(plot_folder, gsub(".obj", ".png", bird_file, fixed = TRUE)))
-        }
+      main_landmarks <- beak_landmarks[1:4, ] %>%
+        as.matrix()
+      
+      mid_beak <- apply(main_landmarks[3:4, ], 2, mean) %>% matrix(nrow = 1)
+      line_1 <- rbind(mid_beak, main_landmarks[1, ])
+      
+      line_1_length <- dist(line_1)
+      line_1_vec <- (line_1[1, , drop = FALSE] - line_1[2, , drop = FALSE]) / line_1_length
+      
+      top_line <- main_landmarks[2, ] - mid_beak
+      
+      tt <- top_line %*% t(line_1_vec)
+      
+      point_on_line_1 <- mid_beak + as.vector(tt) * line_1_vec
+      
+      landmarks_3 <- rbind(main_landmarks[1, ], point_on_line_1, main_landmarks[2, ])
+      landmark_dists <- dist(landmarks_3) %>% as.matrix()
+      target_3 <- matrix(c(landmark_dists[1, 2], 0, 0,
+                           0, 0, 0,
+                           0, 0, landmark_dists[2, 3]),
+                         nrow = 3, byrow = TRUE)
+      
+      L_landmark_folder <- file.path(landmark_folder, "L_landmarks")
+      if(!dir.exists(L_landmark_folder)) {
+        dir.create(L_landmark_folder)
       }
+      
+      all_landmark_folder <- file.path(landmark_folder, "all_landmarks")
+      if(!dir.exists(all_landmark_folder)) {
+        dir.create(all_landmark_folder)
+      }
+      
+      readr::write_csv(as.data.frame(target_3), file.path(L_landmark_folder, gsub(".obj", ".csv", bird_file, fixed = TRUE)),
+                       col_names = FALSE)
+      
+      rot_landmarks <- beak_landmarks %>% as.matrix() %>%
+        Morpho::rotonmat(landmarks_3, target_3)
+      
+      readr::write_csv(as.data.frame(rot_landmarks), file.path(all_landmark_folder, gsub(".obj", ".csv", bird_file, fixed = TRUE)),
+                       col_names = FALSE)
+      
+      rot <- Morpho::rotmesh.onto(beak_mesh, landmarks_3, target_3, adnormals = TRUE)
+      
+      # rgl::shade3d(rot$mesh, alpha = 0.1)
+      # rgl::lines3d(target_3, col = "red")
+      # rgl::points3d(rot_landmarks, size = 4, col = "blue")
+      # rgl::shade3d(sphere, col = "red", alpha = 0.5)
+      
+      ## cut by sphere
+      dist_scale <- dist(target_3[c(1, 3), ])
+      
+      sphere <- Rvcg::vcgSphere(5) %>%
+        rgl::translate3d(target_3[1, 1], target_3[1, 2], target_3[1, 3]) %>%
+        Morpho::scalemesh(dist_scale)
+      
+      clipped <- clip_mesh_by_mesh(rot$mesh, sphere) %>%
+        Rvcg::vcgClean(sel = 0:6, iterate = TRUE, silent = TRUE)
+      
+      # rgl::shade3d(clipped)
+      # rgl::axes3d()
+      # 
+      # rgl::shade3d(scaled)
+      # rgl::axes3d()
+      # rgl::points3d(conv_hull, col = "green")
+      
+      pnts <- Morpho::vert2points(clipped)
+      
+      centroid <- apply(pnts, 2, mean) %>%
+        matrix(nrow = 1)
+      dist_scale <- proxy::dist(centroid, pnts) %>%
+        as.vector() %>%
+        max()
+      
+      scaled <- clipped %>%
+        Morpho::scalemesh(1 / dist_scale, center = "none")
+      
+      pnts <- Morpho::vert2points(scaled)
+      
+      conv_hull <- pnts[unique(as.vector(convhulln(pnts))),]
+        
+      Rvcg::vcgObjWrite(scaled, new_beak_file)
+      readr::write_csv(conv_hull %>% as.data.frame(), hull_file)
+        
     }
   }
   return(invisible(NULL))
 }
+
+library(furrr)
+
+future::plan(future::multisession)
+
+furrr::future_map(beak_files, ~reorient_beak(.x),
+                  .progress = TRUE)
+
+
+#### Find sphere that fits all meshes in it
+library(pbapply)
+hulls <- list.files("data/conv_hulls", full.names = TRUE)
+all_pnts <- pbapply::pblapply(hulls, readr::read_csv) %>%
+  dplyr::bind_rows() %>%
+  as.matrix()
+
+centroid <- apply(all_pnts, 2, mean)
+all_pnts_cent <- t(t(all_pnts) - centroid)
+
+norms <- apply(all_pnts_cent, 1, function(x) sqrt(sum(x^2)))
+max_norm <- max(norms)
+
+## sphere containing all meshes has radius 1.5 and centred at 0.616886145, -0.001077347,  0.145010046
+
+##### Generate SDF samples for all meshes
+library(rtorch)
